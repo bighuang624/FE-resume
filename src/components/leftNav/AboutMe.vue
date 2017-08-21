200dcb0ba45f750f954394d02e6db12b13140547
bighuang624 <kyonhuang@qq.com>
2017-08-21 14:35:57 +0800

    :sparkles:增加项目地址及一些链接

--- src/components/leftNav/AboutMe.vue
+++ src/components/leftNav/AboutMe.vue
@@ -2,9 +2,13 @@
   <section class="aboutMe">
     <h3 class="section-title">关于我</h3>
     <ul>
-      <li v-for="item in info">
+      <li v-for="item in info1">
         <i :class="item.icon"></i>
         <span v-text="item.text"></span>
+      </li>
+      <li v-for="item in info2">
+        <i :class="item.icon"></i>
+        <a v-text="item.text" :href="item.address"></a>
       </li>
     </ul>
   </section>
@@ -15,7 +19,7 @@
     name: 'aboutMe',
     data() {
       return {
-        info: [
+        info1: [
           {
             icon: 'fa fa-graduation-cap',
             title: '教育背景',
@@ -26,15 +30,19 @@
             title: '城市',
             text: '武汉',
           },
+        ],
+        info2: [
           {
             icon: 'fa fa-rss',
             title: '个人博客',
             text: 'kyonhuang.top',
+            address: 'http://kyonhuang.top/',
           },
           {
             icon: 'fa fa-github',
             title: 'github',
             text: '@bighuang624',
+            address: 'https://github.com/bighuang624',
           },
         ],
       };
@@ -47,6 +55,8 @@
   @import "../../assets/css/font-awesome.min.css";
 
   .aboutMe {
-
+    a:hover {
+      color: $common-white;
+    }
   }
 </style>
--- src/components/main/Experience.vue
+++ src/components/main/Experience.vue
@@ -9,8 +9,11 @@
           <h4 v-text="item.name"></h4>
           <span v-text="item.time"></span>
         </div>
-        <div class="experience-intro">
-          <h4 v-text="item.intro"></h4>
+        <div class="experience-detail">
+          <div>
+            <h4 v-text="item.intro"></h4>
+            <a class="experience-address" v-if="item.hasAddress" :href="item.address">项目在线预览</a>
+          </div>
           <span class="experience-description" v-text="item.description"></span>
         </div>
       </li>
@@ -29,18 +32,24 @@
             time: '2016',
             intro: '休闲娱乐社交APP',
             description: '作为队长带队，负责创意的提出、完善，用户需求分析，初步界面设计，未来发展构想等工作并承担信息整合、答辩PPT制作以及现场答辩的任务。最终获得软件创意大赛二等奖。',
+            hasAddress: false,
+            address: '',
           },
           {
             name: '“遇见”',
             time: '2016 - 2017',
             intro: '旅游记录展示空间',
             description: '作为队长带队，担任产品经理和设计师，负责产品定位，用户需求分析，界面设计等工作并承担组内协调、答辩PPT制作以及现场答辩的任务。最终获得“零杯”网页设计大赛一等奖。',
+            hasAddress: false,
+            address: '',
           },
           {
             name: '探方网',
             time: '2017',
             intro: '基于大数据的IP价值评估及预测平台',
             description: '作为前端开发组组长，负责整个项目的前端开发。项目选用了前后端分离的开发模式，所用技术包括且不限于 vue、vue-router、vuex、element、axios、Echarts 等。',
+            hasAddress: false,
+            address: '',
           },
         ],
       };
@@ -74,9 +83,20 @@
     }
   }
 
-  .experience-intro {
+  .experience-detail {
     h4 {
       margin-bottom: 4px;
+      display: inline-block;
+    }
+  }
+
+  .experience-address {
+    margin-left: 16px;
+    font-size: 14px;
+    color: $common-blue;
+
+    &:hover {
+      color: $dark-blue;
     }
   }
 </style>
